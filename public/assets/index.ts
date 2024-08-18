020b9ca93b13df7ae1c2f0d514effcd24b8e4d6c
Sagewiiz <138810243+Sagewiiz@users.noreply.github.com>
2024-08-18 12:20:09 +0530

    Add files via upload

---
+++ public/assets/index.ts
@@ -0,0 +1,8 @@
+import logo from "./images/LOGO1.png";
+import profileImg from "./images/profile1.jpg";
+import amazonImg from "./images/amazonImg.png";
+import cyberImg from "./images/cyber.png";
+import noorShop from "./images/noorShop.png";
+
+export { logo, profileImg, amazonImg, cyberImg, noorShop };
+
