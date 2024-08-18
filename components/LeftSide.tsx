4afdad4ce9ef994db9233f00c6e5f4e5243fa780
Sagewiiz <138810243+Sagewiiz@users.noreply.github.com>
2024-08-18 15:36:53 +0530

    added linkedin

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -16,7 +16,7 @@ const LeftSide = () => {
           </span>
         </a>
         <a
-          href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+          href="https://www.linkedin.com/in/sudhanshu-kumar01/"
           target="_blank"
         >
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
